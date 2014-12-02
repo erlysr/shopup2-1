393c9f0609bbae3da44cefc452e3b59ba348036b
jrperdomoz <jrperdomoz@gmail.com>
2014-12-01 20:34:47 -0800

    update EmailBackend

--- apps/userprofiles/backends.py
+++ apps/userprofiles/backends.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.backends import ModelBackend
 
 
-class EmailBackend(object):
-    def authenticate(self, email=None, password=None):
+class EmailBackend(ModelBackend):
+
+    def authenticate(self, username=None, password=None, **kwargs):
         try:
-            user = User.objects.get(email=email)
+            user = User.objects.get(email=username)
             if user.check_password(password):
                 return user
         except User.DoesNotExist:
