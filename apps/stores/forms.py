0fa0dddec78ddcae98077374dff3ec45f8e595aa
Erly Serrano <erly@3punto14.mx>
2014-12-03 22:37:36 -0600

    form vertical

--- apps/stores/forms.py
+++ apps/stores/forms.py
@@ -36,9 +36,9 @@ class RegStoreStepOneForm(forms.ModelForm):
         self.helper.form_id = 'form-contact'
         self.helper.form_method = 'post'
         self.helper.form_action = ''
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-lg-2'
-        self.helper.field_class = 'col-lg-10'
+        self.helper.form_class = 'form-vertical'
+        #self.helper.label_class = 'col-lg-2'
+        #self.helper.field_class = 'col-lg-10'
         self.helper.layout = Layout(
             Fieldset(
                 'Contacto', 'firstname', 'lastname', 'email', 'contact_phone'
