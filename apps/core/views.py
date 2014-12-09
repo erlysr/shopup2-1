3c705cb17f7b2f601081e8b60abe4d20fb3fa3b8
Erly Serrano <erly@3punto14.mx>
2014-12-08 19:58:54 -0600

    formas de login y profile en ventanas modales

--- apps/core/views.py
+++ apps/core/views.py
@@ -8,3 +8,13 @@ class Home(ListView):
     template_name = 'home.html'
     queryset = Store.objects.filter(status__name='Aprobada')
     paginate_by = 4
+    
+
+    def get_context_data(self, **kwargs):
+    	form_login = forms.EmailAuthenticationForm(request.POST or None)
+    	form_profile = forms.ProfileForm(request.POST or None)
+        context = super(Home, self).get_context_data(**kwargs)
+        context['form_login'] = form_login
+        context['form_profile'] = form_profile
+        
+        return context
