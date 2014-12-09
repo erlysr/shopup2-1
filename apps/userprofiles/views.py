d28de48be17e0bfd23ff7ac6c3e5027acdd6678e
Erly Serrano <erly@3punto14.mx>
2014-12-08 21:38:06 -0600

    home.html loginview

--- apps/userprofiles/views.py
+++ apps/userprofiles/views.py
@@ -113,7 +113,7 @@ class ProfileView(TemplateView):
 class LoginView(FormView):
 
     form_class = forms.EmailAuthenticationForm
-    template_name = 'login.html'
+    template_name = 'home.html'
     success_url = '/'
 
     def form_valid(self, form):
